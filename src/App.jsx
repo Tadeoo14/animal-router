9308c6372e9888651d4ec2be228fbcb7f2edeea0
tadeoo14 <tcardelli@frsn.utn.edu.ar>
2024-09-25 15:36:54 -0300

    added-lazyLoad and Suspence

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
 import { Router, Route, Link, Switch } from 'wouter'
 import './App.css'
 import ListOfAnimals from './components/list-of-animals'
 import AnimalDetail from './components/animal-detail'
+import { lazy, Suspense } from 'react'
+
+//lazy load components
+const AnimalDetailPage = lazy(() => import('./components/animal-detail'))
+const ListOfAnimalsPage = lazy(() => import('./components/list-of-animals'))
 
 function App() {
   
   return (
     <Switch>
+
+
       <Route path="/">
         <h1>Welcome!</h1>
         <Link to="/animals">Go to animals</Link >
       </Route>
-      <Route path="/animals" component={ListOfAnimals}/>
-      <Route path="/animals/:id">
+        <Route path="/animals">
+          <Suspense fallback={<h1>Cargando lista...</h1>}>
+              <ListOfAnimalsPage/>
+          </Suspense>
+        </Route>
+      <Route path="/animals/:id" component={AnimalDetailPage}>
         {(params) => <AnimalDetail animalId={params.id}/>}
       </Route>
 
--- src/components/list-of-animals.jsx
+++ src/components/list-of-animals.jsx
@@ -1,5 +1,6 @@
 import  { Link } from 'wouter';
 import { ANIMALS } from '../../data';
+import { Suspense } from 'react';
 
 export default function ListOfAnimals() {
   return (
@@ -7,7 +8,7 @@ export default function ListOfAnimals() {
         <Link to="/" >Volver a la pagina principal</Link>
         <h2>List Of Animals:</h2>
         <ul>{ANIMALS.map(a =>(
-            <li key={a.id}>
+          <li key={a.id}>
                 <Link to={`/animals/${a.id}`}>{a.name}</Link>
             </li>
         ))}</ul>
